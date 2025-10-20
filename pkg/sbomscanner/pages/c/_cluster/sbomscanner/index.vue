273faa813d5291d127769a87948cc409083229b2
Steven Zhang <steven.zhang@suse.com>
2025-10-19 19:04:09 -0700

    Project name change

---
+++ pkg/sbomscanner/pages/c/_cluster/sbomscanner/index.vue
@@ -0,0 +1,44 @@
+<script>
+import Dashboard from '@pkg/pages/c/_cluster/sbomscanner/Dashboard';
+import { SERVICE, SCHEMA } from '@shell/config/types';
+import { RESOURCE } from '@pkg/types';
+import InstallView from '@pkg/components/InstallView';
+
+export default {
+  name: 'InstallationWizard',
+
+  components: { Dashboard, InstallView },
+
+  async fetch() {
+    if ( this.$store.getters['cluster/canList'](SERVICE) ) {
+      this.allServices = await this.$store.dispatch('cluster/findAll', { type: SERVICE }, { root: true });
+    }
+    if ( this.$store.getters['cluster/canList'](SCHEMA) ) {
+      this.allSchemas = await this.$store.dispatch('cluster/findAll', { type: SCHEMA }, { root: true });
+    }
+  },
+
+  data() {
+    return {
+      allServices: null,
+      allSchemas:  null,
+      index:       -1,
+      store:       this.$store,
+    };
+  },
+
+  computed: {
+    hasSchema() {
+      console.log("this.$store.getters['cluster/schemaFor'](RESOURCE.REGISTRY)", this.$store.getters['cluster/schemaFor'](RESOURCE.REGISTRY))
+      return this.$store.getters['cluster/schemaFor'](RESOURCE.REGISTRY);
+    },
+  }
+};
+</script>
+
+<template>
+  <div v-if="allSchemas">
+    <InstallView v-if="!hasSchema" :hasSchema="hasSchema"/>
+    <Dashboard v-else />
+  </div>
+</template>
