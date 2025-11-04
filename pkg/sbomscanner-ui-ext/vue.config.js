70963adf936e127e3dd5803f7214cc9153284657
Rush Kapoor <rush.kapoor@suse.com>
2025-11-04 09:58:36 -0800

    fix(build): exclude __tests__ to resolve build-pkg errors in sbomscanner-ui-ext
    Instead use rancher/kubewarden-ui strategy: Exclude testing files from Node.js modules are imported

--- pkg/sbomscanner-ui-ext/vue.config.js
+++ pkg/sbomscanner-ui-ext/vue.config.js
@@ -1,26 +1,15 @@
-// Load Rancher’s base Vue config
+const webpack = require('webpack');
 const baseConfig = require('./.shell/pkg/vue.config')(__dirname);
 
 module.exports = {
   ...baseConfig,
-  configureWebpack: (config) => {
-    // Run base Rancher config first
-    if (typeof baseConfig.configureWebpack === 'function') {
-      baseConfig.configureWebpack(config);
-    } else if (typeof baseConfig.configureWebpack === 'object') {
-      Object.assign(config, baseConfig.configureWebpack);
+  chainWebpack: (config) => {
+    if (typeof baseConfig.chainWebpack === 'function') {
+      baseConfig.chainWebpack(config);
     }
 
-    // Explicitly disable or polyfill Node core modules
-    config.resolve = config.resolve || {};
-    config.resolve.fallback = {
-      ...(config.resolve.fallback || {}),
-      fs:     false,
-      module: false,
-      path:   false,
-      os:     false,
-      crypto: false,
-      stream: false,
-    };
+    config.plugin('ignore-tests')
+      .use(webpack.IgnorePlugin, [{ resourceRegExp: /[\\/]__tests__[\\/]/ }]);
   },
+  configureWebpack: baseConfig.configureWebpack,
 };
