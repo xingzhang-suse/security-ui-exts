182d89d234e44bbb4bb8ae61c3199321bf2e1e72
Rush Kapoor <rush.kapoor@suse.com>
2025-11-04 09:11:45 -0800

    fix(build): polyfill Node.js modules to resolve build-pkg errors in sbomscanner-ui-ext

--- pkg/sbomscanner-ui-ext/vue.config.js
+++ pkg/sbomscanner-ui-ext/vue.config.js
@@ -1,1 +1,26 @@
-module.exports = require('./.shell/pkg/vue.config')(__dirname);
+// Load Rancher’s base Vue config
+const baseConfig = require('./.shell/pkg/vue.config')(__dirname);
+
+module.exports = {
+  ...baseConfig,
+  configureWebpack: (config) => {
+    // Run base Rancher config first
+    if (typeof baseConfig.configureWebpack === 'function') {
+      baseConfig.configureWebpack(config);
+    } else if (typeof baseConfig.configureWebpack === 'object') {
+      Object.assign(config, baseConfig.configureWebpack);
+    }
+
+    // Explicitly disable or polyfill Node core modules
+    config.resolve = config.resolve || {};
+    config.resolve.fallback = {
+      ...(config.resolve.fallback || {}),
+      fs:     false,
+      module: false,
+      path:   false,
+      os:     false,
+      crypto: false,
+      stream: false,
+    };
+  },
+};
