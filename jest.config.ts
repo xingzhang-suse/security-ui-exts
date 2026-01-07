be45e83705c3dd6ba9c8535172fc93eaada1b4d7
Steven Zhang <steven.zhang@suse.com>
2026-01-06 21:56:25 -0800

    lint fix

--- jest.config.ts
+++ jest.config.ts
@@ -13,7 +13,7 @@ const config: Config.InitialOptions = {
     '@shell/(.*)': '<rootDir>/node_modules/@rancher/shell/$1',
     '@components/(.*)':
       '<rootDir>/node_modules/@rancher/components/dist/@rancher/components.common.js',
-    '@sbomscanner-ui-ext/(.*)':                     '<rootDir>/pkg/sbomscanner-ui-ext/$1',
+    '@sbomscanner-ui-ext/(.*)':      '<rootDir>/pkg/sbomscanner-ui-ext/$1',
     '@network/(.*)':                 '<rootDir>/pkg/network/$1',
     '@runtime-process-profile/(.*)': '<rootDir>/pkg/runtime-process-profile/$1',
     '@tests/(.*)':                   '<rootDir>/tests/$1',
